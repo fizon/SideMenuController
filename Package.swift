78cbe6d7011644a90df402fdc4393e66d3017adb
Jeremy Boy <github@jboy.eu>
2019-10-09 17:05:55 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "SideMenuController",
+	platforms: [
+		.iOS(.v8)
+	],
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(
+			name: "SideMenuController",
+			targets: ["SideMenuController"]),
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "SideMenuController",
+			dependencies: [],
+			path: "Source"),
+	]
+)
